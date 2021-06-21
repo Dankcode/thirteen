75df67609f3533a7591b09bdfb5c1e1801bb34ab
Codedontwork <68758786+Dankcode@users.noreply.github.com>
2021-06-22 04:15:08 +0800

    Base game
    Game without function, just a card thrower simulator.
    Basic Css for testing

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,6 +4,7 @@ import CreateNewGame from '../Join/CreateNewGame';
 import { Redirect, useParams } from 'react-router-dom';
 import shuffleArray from './Deck/Shuffle';
 import './Game.css';
+import reactDom from 'react-dom';
 const socket  = require('../connections/socket').socket
 
 
@@ -24,81 +25,306 @@ class Game extends React.Component {
 
     state = {
         newGame: false,
-        buttonTest: false,
+        render: false,
         firstCard: false,
+        player: '',
+        winners: [],
         p1Hand: [],
+        p2Hand: [],
+        p3Hand: [],
+        p4Hand: [],
+        pHand: [],
         selectArr: [],
-        removeCard: [],
         playedCard: []
     }
    
-    componentDidMount() {
+    componentDidMount(props) {
         
         const shuffledDeck = shuffleArray(deck)
-
+    
         const player1Deck = shuffledDeck.splice(0, 13)
         const player2Deck = shuffledDeck.splice(0, 13)
         const player3Deck = shuffledDeck.splice(0, 13)
         const player4Deck = shuffledDeck.splice(0, 13)
 
+        player1Deck.sort()
+
         console.log(player1Deck)
         console.log(player2Deck)
         console.log(player3Deck)
         console.log(player4Deck)
+        /*player1Deck.sort(function(a, b) {
+            const suit = player1Deck.charAr(0)
+            const rank = player1Deck.charAr(1)
+            var j;
+                for (j = 0; j < player1Deck.length;j  ++){
+                switch (suit)
+                {
+                default: 
+                return null;
+                case 's':
+                return 10;
+                case 'c':
+                return 20;
+                case 'd':
+                return 30;
+                case 'h':
+                return 40; 
+                }
+            }
+                return null
+                
+                })
+*/
+this.setState({
+    p1Hand : player1Deck,
+    p2Hand : player2Deck,
+    p3Hand : player3Deck,
+    p4Hand : player4Deck,
+}) 
 
-//NEED update win conditions
 
 // need to order the deck eventually 
-this.setState({
-    p1Hand : player1Deck
-})
-   
-// create StartingCard that searches for 3 of Spades
-//extract card from Searching player decks that player 2nd player becomes first to play 
-// const a first turn 
 
 socket.emit('initGameState', {
-    gameOver: false,
+    newGame: false,
     turn: 'Player 1',
     player1Deck: [...player1Deck],
     player2Deck: [...player2Deck],
-  //  currentColor: playedCardsPile[0].charAt(1),
-   // currentNumber: playedCardsPile[0].charAt(0),
-    //playedCardsPile: [...playedCardsPile],
-   // drawCardPile: [...drawCardPile]
+    player3Deck: [...player3Deck],
+    player4Deck: [...player4Deck],
 })}
     
-renderCard = () => {
+p1Seat = () => {
+    const playerHand = this.state.p1Hand
 this.setState({
-    buttonTest: true,
+    pHand: playerHand,
+    player: 'player1'
 })}      
-
-/*
-selectCard = (selected_card) => {
-    // will have to check if cardplay is Legal
-    if (!this.state.selectArr.includes(selected_card)) {
-        this.state.selectArr.push(selected_card);
-        console.log(selected_card)
-    } else {
-        var j;
-            for (j = 0; j < this.state.selectArr.length;j  ++)
-                if (selected_card === this.state.selectArr[j]) {
-                    this.state.selectArr.splice(j, 1);
-                }
+p2Seat = () => {
+    const playerHand = this.state.p2Hand
+this.setState({
+    pHand: playerHand,
+    player: 'player2'
+})}      
+p3Seat = () => {
+    const playerHand = this.state.p3Hand
+    this.setState({
+        pHand: playerHand,
+        player: 'player3'
+    })}    
+p4Seat = () => {
+    const playerHand = this.state.p4Hand
+this.setState({
+    pHand: playerHand,
+    player: 'player4'
+})}    
+otherDeck = () => {
+    switch (this.state.player){
+    case 'player1':
+        return ( 
+            <div>
+            <div className = 'right'>
+                {this.state.p2Hand.map((item, i) => (
+                                    <img
+                                        alt = ''
+                                        key={i}
+                                        className='smallCard'
+                                        src={require(`./Deck/assets/back.png`).default}
+                                        />
+                    ))}
+            </div>
+            <div className = 'top'>
+                {this.state.p3Hand.map((item, i) => (
+                                    <img
+                                        alt = ''
+                                        key={i}
+                                        className='smallCard'
+                                        src={require(`./Deck/assets/back.png`).default}
+                                        />
+                    ))}
+            </div>
+            <div className = 'left'>
+                {this.state.p4Hand.map((item, i) => (
+                                    <img
+                                        alt = ''
+                                        key={i}
+                                        className='smallCard'
+                                        src={require(`./Deck/assets/back.png`).default}
+                                        />
+                    ))}
+            </div>
+            </div>
+        )
+    case 'player2':
+        return ( 
+            <div>
+            <div className = 'right'>
+                {this.state.p3Hand.map((item, i) => (
+                                    <img
+                                        alt = ''
+                                        key={i}
+                                        className='smallCard'
+                                        src={require(`./Deck/assets/back.png`).default}
+                                        />
+                    ))}
+            </div>
+            <div className = 'top'>
+                {this.state.p4Hand.map((item, i) => (
+                                    <img
+                                        alt = ''
+                                        key={i}
+                                        className='smallCard'
+                                        src={require(`./Deck/assets/back.png`).default}
+                                        />
+                    ))}
+            </div>
+            <div className = 'left'>
+                {this.state.p1Hand.map((item, i) => (
+                                    <img
+                                        alt = ''
+                                        key={i}
+                                        className='smallCard'
+                                        src={require(`./Deck/assets/back.png`).default}
+                                        />
+                    ))}
+            </div>
+            </div>
+        )
+    case 'player3':
+        return ( 
+            <div>
+            <div className = 'right'>
+                {this.state.p4Hand.map((item, i) => (
+                                    <img
+                                        alt = ''
+                                        key={i}
+                                        className='smallCard'
+                                        src={require(`./Deck/assets/back.png`).default}
+                                        />
+                    ))}
+            </div>
+            <div className = 'top'>
+                {this.state.p1Hand.map((item, i) => (
+                                    <img
+                                        alt = ''
+                                        key={i}
+                                        className='smallCard'
+                                        src={require(`./Deck/assets/back.png`).default}
+                                        />
+                    ))}
+            </div>
+            <div className = 'left'>
+                {this.state.p2Hand.map((item, i) => (
+                                    <img
+                                        alt = ''
+                                        key={i}
+                                        className='smallCard'
+                                        src={require(`./Deck/assets/back.png`).default}
+                                        />
+                    ))}
+            </div>
+            </div>
+        )
+    case 'player4':
+        return ( 
+            <div>
+            <div className = 'right'>
+                {this.state.p1Hand.map((item, i) => (
+                                    <img
+                                        alt = ''
+                                        key={i}
+                                        className='smallCard'
+                                        src={require(`./Deck/assets/back.png`).default}
+                                        />
+                    ))}
+            </div>
+            <div className = 'top'>
+                {this.state.p2Hand.map((item, i) => (
+                                    <img
+                                        alt = ''
+                                        key={i}
+                                        className='smallCard'
+                                        src={require(`./Deck/assets/back.png`).default}
+                                        />
+                    ))}
+            </div>
+            <div className = 'left'>
+                {this.state.p3Hand.map((item, i) => (
+                                    <img
+                                        alt = ''
+                                        key={i}
+                                        className='smallCard'
+                                        src={require(`./Deck/assets/back.png`).default}
+                                        />
+                    ))}
+            </div>
+            </div>
+        )
+    default: return ( 
+        <div>
+        <div className = 'right'>
+            {this.state.p2Hand.map((item, i) => (
+                                <img
+                                    alt = ''
+                                    key={i}
+                                    className='smallCard'
+                                    src={require(`./Deck/assets/back.png`).default}
+                                    />
+                ))}
+        </div>
+        <div className = 'top'>
+            {this.state.p3Hand.map((item, i) => (
+                                <img
+                                    alt = ''
+                                    key={i}
+                                    className='smallCard'
+                                    src={require(`./Deck/assets/back.png`).default}
+                                    />
+                ))}
+        </div>
+        <div className = 'left'>
+            {this.state.p4Hand.map((item, i) => (
+                                <img
+                                    alt = ''
+                                    key={i}
+                                    className='smallCard'
+                                    src={require(`./Deck/assets/back.png`).default}
+                                    />
+                ))}
+        </div>
+        <div className = 'bottom'>
+            {this.state.p1Hand.map((item, i) => (
+                                <img
+                                    alt = ''
+                                    key={i}
+                                    className='smallCard'
+                                    src={require(`./Deck/assets/back.png`).default}
+                                    />
+                ))}
+        </div>
+        </div>
+    )
     }
 }
-*/
+changeSeat = () => {
+    const quit = this.state.selectArr.splice(0, 13)
+this.state.pHand.push(quit)
+    this.setState({
+        player: ''
+    })
+}
 selectCard = (selected_card) => {
     // will have to check if cardplay is Legal
     this.state.selectArr.push(selected_card);
         console.log(selected_card)
     
         var i;
-        for (i = 0; i < this.state.p1Hand.length; i ++) {
+        for (i = 0; i < this.state.pHand.length; i ++) {
             var j;
                 for (j = 0; j < this.state.selectArr.length;j  ++)
-                    if (this.state.p1Hand[i] === this.state.selectArr[j]) {
-                        this.state.p1Hand.splice(i, 1);
+                    if (this.state.pHand[i] === this.state.selectArr[j]) {
+                        this.state.pHand.splice(i, 1);
           }
         }
     const selectedCard = this.state.selectArr
@@ -108,20 +334,20 @@ selectCard = (selected_card) => {
 }
 
 deselectCard = (selected_card) => {
-    this.state.p1Hand.push(selected_card);
+    this.state.pHand.push(selected_card);
         console.log(selected_card)
     
         var i;
         for (i = 0; i < this.state.selectArr.length; i ++) {
             var j;
-                for (j = 0; j < this.state.p1Hand.length;j  ++)
-                    if (this.state.selectArr[i] === this.state.p1Hand[j]) {
+                for (j = 0; j < this.state.pHand.length;j  ++)
+                    if (this.state.selectArr[i] === this.state.pHand[j]) {
                         this.state.selectArr.splice(i, 1);
           }
         }
-    const selectedCard = this.state.p1Hand
+    const selectedCard = this.state.pHand
     this.setState({
-        p1Hand: selectedCard
+        pHand: selectedCard
     })
 }
 
@@ -136,42 +362,16 @@ playButton = () => {
         playedCard: uniqueCards,
     })
 }
-/*
-removeHand = () => {
-    var i;
-    for (i = 0; i < this.state.p1Hand.length; i ++) {
-        var j;
-            for (j = 0; j < this.state.selectArr.length;j  ++)
-                if (this.state.p1Hand[i] === this.state.selectArr[j]) {
-                    this.state.p1Hand.splice(i, 1);
-      }
-    }
-
-    console.log(this.state.p1Hand)
-    console.log(this.state.selectArr)
-  }
-*/
-    
-
-    //last cards played
-    
-        
-        // track player that played the cards
-        //extract the number and rank of the played cards
-
-        //check if the card is legal 
-        //remove the card from the according player's deck
-        // exception for BOMB which is allowed anytime
-
-    
 
 render() {
     return(
         <div>
-            <h2>
+            <div className = 'otherDeck'>
+            {this.otherDeck()}
+            </div>
                 <div className='Square'>
             
-                        <div>
+                <div>
                 {this.state.playedCard.map((item, i) => (
                                 <img
                                     alt = ''
@@ -181,10 +381,11 @@ render() {
                                     />
                 ))}
                 </div>
-             
+                
                 </div>
+    <React.Fragment>           
             <React.Fragment>
-                {this.state.buttonTest ?
+                {this.state.player === 'player1' || this.state.player ==='player2' || this.state.player ==='player3' || this.state.player ==='player4'?
                 <div>
                     <button onClick={() => this.playButton()}> PLAY SELECTED </button>
                 <div>
@@ -199,7 +400,7 @@ render() {
                                     />
                 ))}
                 </div>
-                {this.state.p1Hand.map((item, i) => (
+                {this.state.pHand.map((item, i) => (
                                 <img
                                     alt = ''
                                     key={i}
@@ -208,16 +409,25 @@ render() {
                                     onClick={() => this.selectCard(item)}
                                     />
                 ))}
+                
+            <button onClick={() => this.changeSeat()}> CHANGE SEAT</button>
                 </div>
                 </div>
+                
                 :             
                 <div>
-                    hi
+                    SELECT SEAT
                 </div>
                 }
             </React.Fragment>
-            <button onClick={() => this.renderCard()}> add card </button>
-            </h2>
+<div>
+            <button onClick={() => this.p1Seat()}> Player 1 </button>
+            <button onClick={() => this.p2Seat()}> Player 2 </button>
+            <button onClick={() => this.p3Seat()}> Player 3 </button>
+            <button onClick={() => this.p4Seat()}> Player 4 </button>
+</div>
+</React.Fragment> 
+            
         </div>
     )
     }
